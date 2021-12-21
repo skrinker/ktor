73dd707cefb252dc1735306b61a8cb5e84c2a5e2
skrinker <guzel28sit@gmail.com>
2021-12-21 07:22:53 +0300

    fix posts interface

--- src/models/Post.kt
+++ src/models/Post.kt
@@ -5,10 +5,3 @@ data class Post(
     val postId: Int,
     val text: String
 )
-
-interface PostRepo {
-    suspend fun publishPost(uid: Int, text: String): Post?
-    suspend fun getPosts(uid: Int): List<Post>
-    suspend fun deletePost(userId: Int, postId: Int): Int
-    suspend fun updatePost(userId: Int, postId: Int, post: PostDomain): Int
-}
